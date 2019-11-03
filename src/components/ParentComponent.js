92f946c8e970b9f94091abb24beec28c88523710
Tammy Metz <tammy@tammymetz.com>
2019-11-03 19:29:36 +0000

    Automatically backed up by Learn

--- src/components/ParentComponent.js
+++ src/components/ParentComponent.js
@@ -1,24 +1,24 @@
 import React from 'react';
 import Form from './Form'
- 
+
 class ParentComponent extends React.Component {
   state = {
     firstName: "",
     lastName: "",
   }
- 
+
   handleFirstNameChange = event => {
     this.setState({
       firstName: event.target.value
     })
   }
- 
+
   handleLastNameChange = event => {
     this.setState({
       lastName: event.target.value
     })
   }
- 
+
   render() {
     return (
       <Form
@@ -29,5 +29,5 @@ class ParentComponent extends React.Component {
     )
   }
 }
- 
+
 export default ParentComponent;
