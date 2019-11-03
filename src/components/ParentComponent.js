3a7cbe0c97775736d7062f70815e8752184a2746
Tammy Metz <tammy@tammymetz.com>
2019-11-03 19:35:54 +0000

    Done.

---
+++ src/components/ParentComponent.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import Form from './Form'
+import DisplayData from './DisplayData'
+
+class ParentComponent extends React.Component {
+  state = {
+    firstName: "",
+    lastName: "",
+  }
+
+  handleFirstNameChange = event => {
+    this.setState({
+      firstName: event.target.value
+    })
+  }
+
+  handleLastNameChange = event => {
+    this.setState({
+      lastName: event.target.value
+    })
+  }
+
+  handleChange = event => {
+  this.setState({
+    [event.target.name]: event.target.value
+  })
+}
+
+  render() {
+    return (
+      <div>
+        <Form
+          formData={this.state}
+          handleChange={this.handleChange}
+        />
+        <DisplayData formData={this.state} />
+      </div>
+    )
+  }
+}
+
+export default ParentComponent;
